4c9fa77cc80ddeeab49af4fe0c554d980224681e
James Tiplady <biggleszx@biggleszx.com>
2015-12-26 23:42:50 +0000

    Fixing up a few bugs

--- sysprep.sh
+++ sysprep.sh
@@ -11,14 +11,15 @@
 # |               https://github.com/BigglesZX/sysprep.sh                      |
 # `----------------------------------------------------------------------------'
 
+echo -e "\033[0;32msysprep.sh\033[0m"
+
 # we need to be root
 if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root." 1>&2
+   echo "Sorry! This script must be run as root." 1>&2
    exit 1
 fi
 
 # prompt
-echo -e "\033[0;32msysprep.sh\033[0m"
 echo "This script will perform initial setup actions on this server."
 read -r -p "Type 'Y' to continue or anything else to abort: " GO
 if [ "$GO" != "Y" ]; then
@@ -27,11 +28,11 @@ if [ "$GO" != "Y" ]; then
 fi
 
 # set timezone
-echo "Setting timezone..."
+echo " * Setting timezone..."
 dpkg-reconfigure tzdata
 
 # set root password
-echo "Setting root password..."
+echo " * Setting root password..."
 passwd
 
 # ask for new standard user's username
@@ -42,42 +43,42 @@ if [ -z "$USERNAME" ]; then
 fi
 
 # add regular user
-echo "Creating standard user..."
+echo " * Creating standard user..."
 useradd -s /bin/bash -d /home/$USERNAME -m -U $USERNAME
 passwd $USERNAME
 
 # copy SSH authorized_keys
-echo "Copying SSH public key to $USERNAME's home directory..."
+echo " * Copying SSH public key to $USERNAME's home directory..."
 mkdir /home/$USERNAME/.ssh
 chown $USERNAME.$USERNAME /home/$USERNAME/.ssh/
 cp /root/.ssh/authorized_keys /home/$USERNAME/.ssh/authorized_keys
 chown $USERNAME.$USERNAME /home/$USERNAME/.ssh/authorized_keys
 
 # set sensible prompt
-echo "Setting up bash prompt..."
+echo " * Setting up bash prompt..."
 echo "PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\\$ '" >> /home/$USERNAME/.bashrc
 
 # add $USERNAME to sudoers
-echo "Adding $USERNAME to sudoers with ALL privileges..."
+echo " * Adding $USERNAME to sudoers with ALL privileges..."
 echo "$USERNAME ALL=(ALL:ALL) ALL" > /etc/sudoers.d/sysprep
 chmod 0440 /etc/sudoers.d/sysprep
 
 # python setup
-echo "Setting up python..."
+echo " * Setting up python..."
 apt-get update
 apt-get install python-setuptools
 easy_install pip
 pip install ipdb ipython virtualenv virtualenvwrapper
 
 # virtualenv .bashrc stuff
-echo "Completing virtualenv config for $USERNAME..."
+echo " * Completing virtualenv config for $USERNAME..."
 echo "WORKON_HOME=\$HOME/.virtualenvs" >> /home/$USERNAME/.bashrc
 echo "export PROJECT_HOME=\$HOME/sites" >> /home/$USERNAME/.bashrc
 echo "#export VIRTUALENV_DISTRIBUTE=true" >> /home/$USERNAME/.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> /home/$USERNAME/.bashrc
 
 # swapfile setup
-echo "Adding swapfile..."
+echo " * Adding swapfile..."
 dd if=/dev/zero of=/swapfile bs=1024 count=512k
 mkswap /swapfile
 swapon /swapfile
@@ -88,28 +89,28 @@ chown root:root /swapfile
 chmod 0600 /swapfile
 
 # iptables setup
-echo "Setting up iptables with standard web server config..."
-apt-get install iptables-persistent
-service iptables-persistent start
+echo " * Setting up iptables with standard web server config..."
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -P INPUT DROP
 iptables -I INPUT 1 -i lo -j ACCEPT
+apt-get install iptables-persistent
+service iptables-persistent start
 iptables-save > /etc/iptables/rules.v4
 
 # install other common packages
-echo "Installing other common apt packages..."
+echo " * Installing other common apt packages..."
 apt-get install git ntp python-dev nginx mysql-server mysql-client libmysqlclient-dev memcached python-memcache
 
 # mysql timezone loading
-echo "Loading timezone data into mysql..."
-mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root mysql
+echo " * Loading timezone data into mysql..."
+mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root -p mysql
 service mysql restart
 
 # install Pillow dependencies
-echo "Installing Pillow dependencies..."
+echo " * Installing Pillow dependencies..."
 apt-get install libjpeg-dev libjpeg8-dev libpng3 libfreetype6-dev
 ln -s /usr/lib/`uname -i`-linux-gnu/libfreetype.so /usr/lib
 ln -s /usr/lib/`uname -i`-linux-gnu/libjpeg.so /usr/lib
@@ -117,8 +118,9 @@ ln -s /usr/lib/`uname -i`-linux-gnu/libz.so /usr/lib
 ln -s /usr/include/freetype2 /usr/local/include/freetype
 
 # cleanup
-echo "Cleaning up..."
+echo " * Cleaning up..."
+apt-get autoremove
 apt-get clean
 
 # done
-echo "Done!"
+echo "Done! Enjoy."
