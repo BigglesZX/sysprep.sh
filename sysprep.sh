c053d8f91bf7ecb93babae81dd9f8d2a940f36f1
James Tiplady <biggleszx@biggleszx.com>
2016-12-22 00:35:03 +0000

    Adding lxml deps to apt installation

--- sysprep.sh
+++ sysprep.sh
@@ -104,7 +104,7 @@ iptables-save > /etc/iptables/rules.v4
 
 # install other common packages
 echo " * Installing other common apt packages..."
-apt-get install git ntp python-dev nginx mysql-server mysql-client libmysqlclient-dev memcached python-memcache htop libffi-dev
+apt-get install git ntp python-dev nginx mysql-server mysql-client libmysqlclient-dev memcached python-memcache htop libffi-dev libxml2-dev libxslt1-dev
 
 # mysql timezone loading
 echo " * Loading timezone data into mysql..."
