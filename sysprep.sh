052e65b67933d06099ff8d5a037f0316f3b0c9e6
James Tiplady <biggleszx@biggleszx.com>
2017-02-11 12:08:55 +0000

    Adding Diffie-Hellman profile generation as precursor to Nginx SSL setup

--- sysprep.sh
+++ sysprep.sh
@@ -106,6 +106,9 @@ iptables-save > /etc/iptables/rules.v4
 echo " * Installing other common apt packages..."
 apt-get install git ntp python-dev nginx mysql-server mysql-client libmysqlclient-dev memcached python-memcache htop libffi-dev libxml2-dev libxslt1-dev python-lxml fail2ban
 
+# generate Diffie-Hellman profile
+openssl dhparam -out /etc/ssl/nginx/dhparam.pem 2048
+
 # set up apt unattended upgrades
 dpkg-reconfigure --priority=low unattended-upgrades
 
